555bac482d9664e673d8727afc23f62cb46f16ca
ccj <3301375362@qq.com>
2026-04-06 23:13:37 +0800

    重大的阶段性突破
    记账本逻辑已经完全完善，学ui的三个挑战已经成功，开始winforms学习ui了！

--- 正式写软件前的逻辑训练与小作品/简易猜数字逻辑.cs
+++ 正式写软件前的逻辑训练与小作品/简易猜数字逻辑.cs
@@ -1,60 +1,154 @@
 ﻿using System;
-namespace RectangleApplication
-{
-    class Model
-    {
-        public static bool judgement(int x, int y)
-        {
-            return x == y;
-        }
-        public static int trans()
-        {
-            int c;
-            while (!int.TryParse(Console.ReadLine(), out c))
-            {
-                Console.WriteLine("请重新输入一个自然数");
-            }
-            return c;
-        }
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Policy;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
-    }
-    class program
+namespace 逻辑训练3_记账本
+{
+    class Date//数据录入
     {
-        static void Main()
+        public decimal a;
+        public string b;
+        public Date(decimal a, string b)
         {
-            Random a = new Random();
-            Console.WriteLine("欢迎来到逻辑测训练项目——猜数字");
-            while(true)
+            this.a = a;
+            this.b = b;
+        }
+    }
+    class Function
+    {
+        public static void inrecord(List<Date> z)//记录账单
+        {
+            Console.WriteLine("本次是记录入账还是花销？");
+
+            decimal a;
+            string b;
+            while (true)
             {
-                int b=a.Next(1,3);
-                Console.WriteLine("请输入你猜测的自然数");
-                int c;
-                while (true)
+                Console.WriteLine("1：入账     2：花销");
+                string choose = Console.ReadLine();
+                if (choose == "1")
                 {
-                    c = Model.trans();
-                    if (Model.judgement(b, c))
+                    Console.WriteLine("请输入具体金额");
+                    while (!decimal.TryParse(Console.ReadLine(), out a))
                     {
-                        Console.WriteLine("真棒，你猜对了");
-                        Console.ReadKey();
-                        break;
+                        Console.WriteLine("请重新输入正常数字");
                     }
-                    else
-                    {
-                        Console.WriteLine("猜错了，请重新再来");
-                    }
+                    Console.WriteLine("备注");
+                    b = Console.ReadLine();
+                    break;
                 }
-                Console.WriteLine("请问是否还想再来一遍？");
-                Console.WriteLine("输入数字1并按下回车即可重置本程序运行，或直接按下回车键退出本程序");
-                if(Console.ReadLine() == "1")
+                else if (choose == "2")
                 {
-                    Console.WriteLine("正在重置随机数，按下回车键即可重新开始");
-                    Console.ReadKey();
+                    Console.WriteLine("请输入具体金额");
+                    while (!decimal.TryParse(Console.ReadLine(), out a))
+                    {
+                        Console.WriteLine("请重新输入正常数字");
+                    }
+                    a = -a;
+                    Console.WriteLine("备注");
+                    b = Console.ReadLine();
+                    break;
                 }
                 else
                 {
+                    Console.WriteLine("请重新输入正确的选择");
+                }
+            }
+            z.Add(new Date(a, b));
+        }
+        public static void summarize(List<Date> money)//当前总结
+        {
+            decimal x = 0;
+            decimal y = 0;
+            foreach (var item in money)
+            {
+                if (item.a < 0)
+                    y += item.a;
+                else
+                    x += item.a;
+            }
+            decimal m = x + y;
+            Console.WriteLine("当前总收入为" + x + "元");
+            Console.WriteLine("当前总支出为" + y + "元");
+            Console.WriteLine("当前总收支为" + m + "元");
+            Console.ReadKey();
+            Console.WriteLine("请问是否要查询已产生的每笔收支明细");
+            Console.WriteLine("按下数字1并回车即可查询，或者按下回车直接结束本次小结");
+            string choose = Console.ReadLine();
+            if (choose == "1")
+            {
+                Console.WriteLine("当前总计产生" + money.Count + "笔收支项目，内容如下");
+                for (int e = 0; e < money.Count; e++)
+                {
+                    int a = e + 1;
+                    Console.WriteLine("第" + a + "笔；" + money[e].a);
+                    Console.WriteLine("备注：" + money[e].b);
+                }
+            }
+            Console.WriteLine("按下回车结束本次小结");
+            Console.ReadKey();
+        }
+        public static void day(List<Date> money)//每日总结
+        {
+            decimal x = 0;
+            decimal y = 0;
+            int i = money.Count;
+            foreach (var item in money)
+            {
+                if (item.a < 0)
+                    y += item.a;
+                else
+                    x += item.a;
+            }
+            decimal m = x + y;
+            Console.WriteLine("今日总收入为" + x + "元");
+            Console.WriteLine("今日总指出为" + y + "元");
+            Console.WriteLine("今日总收支为" + m + "元");
+            Console.ReadKey();
+            Console.WriteLine("今日总计产生" + i + "笔收支项目，内容如下");
+            for (int e = 0; e < money.Count; e++)
+            {
+                int a = e + 1;
+                Console.WriteLine("第" + a + "笔；" + money[e].a);
+                Console.WriteLine("备注：" + money[e].b);
+            }
+            Console.ReadKey();
+            Console.WriteLine("按下回车结束今日总结并关闭程序");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Date> money = new List<Date>();
+            while (true)
+            {
+                Console.WriteLine("您是想记录账单，还是立刻总结当前的花销，又或是直接开始总结今日的收支？");
+                Console.WriteLine("输入数字1并按下回车键，即可记录当前账单");
+                Console.WriteLine("输入数字2并按下回车键，即可立刻总结当前花销");
+                Console.WriteLine("直接按下回车键，即可直接开始总结今日的收支");
+                string choose = Console.ReadLine();
+                if (choose == "1")
+                {
+                    Function.inrecord(money);
+                }
+                else if (choose == "2")
+                {
+                    Function.summarize(money);
+                }
+                else
+                {
+                    Console.WriteLine("正在总结中");
+                    Thread.Sleep(3000);
                     break;
                 }
             }
+            Function.day(money);
         }
     }
 }
